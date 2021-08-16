2ede0fd743824672eb211b2d9d3853bd1d8db204
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-16 16:17:25 +0530

    Update Scopes.js

--- Scopes.js
+++ Scopes.js
@@ -1,9 +1,36 @@
 
-
 function a(){
     console.log(b);
 }
 var b = 10;
 a(); // 10
 
+function a(){
+    function c(){
+        console.log(b);
+    }
+    c();
+}
+var b = 10;
+a(); // 10
 
+function a(){
+    var b = 10;
+    function c(){
+        console.log(b);
+    }
+    c();
+}
+a(); // 10
+
+
+
+function a(){
+    var b = 10;
+    function c(){
+        
+    }
+    c();
+}
+a(); // 10
+console.log(b); // reference error - b is not defined
