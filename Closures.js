202aa3d9726c339793e6630be2d79a36b4068b59
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-16 18:08:11 +0530

    Updated about closure functions

---
+++ Closures.js
@@ -0,0 +1,60 @@
+
+function x(){
+    var a = 5;
+    function y(){
+        console.log(a);
+    }
+    y();
+}
+// 5
+
+
+function x(){
+    var a = 5;
+    function y(){
+        console.log(a);
+    }
+    return y;
+}
+
+// or 
+
+function x(){
+    var a = 5;
+    return function y(){
+        console.log(a);
+    }
+}
+
+const z = x();
+// y(){ console.log(a); }
+z();
+// 5
+
+
+function x(){
+    var a = 5;
+    function y(){
+        console.log(a); // closure has a's reference not exact value (currently=5)
+    }
+    a = 77;
+    return y;
+}
+const z = x();
+z();
+// 77
+
+
+function z(){
+    var b = 6;
+    function x(){
+    var a = 5;
+        function y(){
+            console.log(b, a);
+        }
+        y();
+    }
+    x();
+}
+z();
+// 6, 5
