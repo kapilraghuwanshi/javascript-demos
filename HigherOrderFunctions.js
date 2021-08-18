613ee3d19743520a041a433cbf49d4b0106ee026
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-18 15:21:29 +0530

    Update HigherOrderFunctions.js

--- HigherOrderFunctions.js
+++ HigherOrderFunctions.js
@@ -9,3 +9,24 @@ const euro = (value) => "€" + value;
 
 withCurrencyPrefix(45, rupee);  //₹45
 withCurrencyPrefix(23, dollar); //$23
+
+
+// higher order function
+function calculate(radius, formula) {
+    console.log(formula(radius));
+}
+
+//callback functions
+function diameter(param){
+    return 2*Math.PI*param;
+}
+function area(param){
+    return Math.PI*param*param;
+}
+
+
+calculate(3, diameter);
+// 18.84955592153876
+
+calculate(3, area);
+// 28.274333882308138
