5acbf627c8ae9ab4dde3252405f0db450ed7b8b8
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-18 15:11:16 +0530

    Update HigherOrderFunctions.js

--- HigherOrderFunctions.js
+++ HigherOrderFunctions.js
@@ -1,1 +1,11 @@
 
+// higher order function
+const withCurrencyPrefix = (value, formatter) => formatter(value);
+
+//callback functions
+const rupee = (value) => "₹" + value;
+const dollar = (value) => "$" + value;
+const euro = (value) => "€" + value;
+
+withCurrencyPrefix(45, rupee);  //₹45
+withCurrencyPrefix(23, dollar); //$23
