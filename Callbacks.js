f7f3e2c795943f17f6f0bfc7bfc5ffc3b9c4816e
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2021-08-18 12:46:55 +0530

    Update Callbacks.js

--- Callbacks.js
+++ Callbacks.js
@@ -1,1 +1,24 @@
 
+
+setTimeout(function (){
+    console.log("setTime");
+}, 5000);
+
+function x(y){
+    console.log("x");
+    y();
+}
+
+x(function y(){
+console.log("y");
+});
+
+// x
+// y
+// stack empty
+// after 5 seconds
+// stack has one element async
+// setTime
+
+
+
